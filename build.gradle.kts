9202242d91ea680fb3a8403d903c6a7b7d0e0966
reocat <111279389+reocat@users.noreply.github.com>
2024-07-27 01:15:46 +0300

    Bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 allprojects {
     apply(from = "${rootProject.projectDir}/repositories.gradle.kts")
+
+    // Set Java version for Java tasks
+    tasks.withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
+    // Set Java version for Kotlin tasks
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
